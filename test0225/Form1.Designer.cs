cf802fa43f8b43d19800bbeec6e8c1531776d7f8
jack_xu <jack_xu@JACK>
2017-02-25 11:37:19 +0800

    0225

--- test0225/Form1.Designer.cs
+++ test0225/Form1.Designer.cs
@@ -28,12 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonX1 = new DevComponents.DotNetBar.ButtonX();
+            this.calendarView1 = new DevComponents.DotNetBar.Schedule.CalendarView();
+            this.SuspendLayout();
+            // 
+            // buttonX1
+            // 
+            this.buttonX1.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX1.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.buttonX1.Location = new System.Drawing.Point(74, 45);
+            this.buttonX1.Name = "buttonX1";
+            this.buttonX1.Size = new System.Drawing.Size(75, 23);
+            this.buttonX1.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.buttonX1.TabIndex = 0;
+            this.buttonX1.Text = "buttonX1";
+            // 
+            // calendarView1
+            // 
+            // 
+            // 
+            // 
+            this.calendarView1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.calendarView1.ContainerControlProcessDialogKey = true;
+            this.calendarView1.Location = new System.Drawing.Point(193, 45);
+            this.calendarView1.MultiUserTabHeight = 20;
+            this.calendarView1.Name = "calendarView1";
+            this.calendarView1.Size = new System.Drawing.Size(320, 187);
+            this.calendarView1.TabIndex = 1;
+            this.calendarView1.Text = "calendarView1";
+            this.calendarView1.TimeIndicator.BorderColor = System.Drawing.Color.Empty;
+            this.calendarView1.TimeIndicator.Tag = null;
+            this.calendarView1.TimeSlotDuration = 30;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 262);
+            this.Controls.Add(this.calendarView1);
+            this.Controls.Add(this.buttonX1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private DevComponents.DotNetBar.ButtonX buttonX1;
+        private DevComponents.DotNetBar.Schedule.CalendarView calendarView1;
     }
 }
 
